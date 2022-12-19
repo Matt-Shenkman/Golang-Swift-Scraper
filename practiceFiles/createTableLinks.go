df88c5fa7d56ace5b049b652c6773921f596ec11
Matt-Shenkman <shenkman@seas.upenn.edu>
2022-12-19 00:01:31 -0500

    fixing issues

--- practiceFiles/createTableLinks.go
+++ practiceFiles/createTableLinks.go
@@ -1,44 +1,44 @@
-// package main
+package main
 
-// import (
-// 	"encoding/csv"
-// 	"fmt"
-// 	"log"
-// 	"os"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
 
-// 	"github.com/gocolly/colly"
-// )
+	"github.com/gocolly/colly"
+)
 
-// func main() {
-// 	fName := "artists.csv"
-// 	file, err := os.Create(fName)
-// 	if err != nil {
-// 		log.Fatalf("Could not create file, err: %q", err)
-// 		return
-// 	}
-// 	defer file.Close()
+func main() {
+	fName := "artists.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Could not create file, err: %q", err)
+		return
+	}
+	defer file.Close()
 
-// 	writer := csv.NewWriter(file)
-// 	defer writer.Flush()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-// 	c := colly.NewCollector()
-// 	c.OnRequest(func(r *colly.Request) { fmt.Println("Scraping:", r.URL) })
-// 	c.OnResponse(func(r *colly.Response) { fmt.Println("Status:", r.StatusCode) })
-// 	c.OnError(func(r *colly.Response, err error) {
-// 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "nError:", err)
-// 	})
+	c := colly.NewCollector()
+	c.OnRequest(func(r *colly.Request) { fmt.Println("Scraping:", r.URL) })
+	c.OnResponse(func(r *colly.Response) { fmt.Println("Status:", r.StatusCode) })
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "nError:", err)
+	})
 
-// 	// c.OnHTML("*", func(e *colly.HTMLElement) {
-// 	// 	fmt.Println(e)
-// 	// })
+	// c.OnHTML("*", func(e *colly.HTMLElement) {
+	// 	fmt.Println(e)
+	// })
 
-// 	// c.OnHTML("a.sc-dIvrsQ", func(e *colly.HTMLElement) {
-// 	// 	fmt.Println(e.Text)
-// 	// })
+	// c.OnHTML("a.sc-dIvrsQ", func(e *colly.HTMLElement) {
+	// 	fmt.Println(e.Text)
+	// })
 
-// 	c.OnHTML(("div.sc-jYKCQm.fidnen"), func(e *colly.HTMLElement) {
-// 		fmt.Println("here")
-// 	})
-// 	c.Visit("https://www.stubhub.com/taylor-swift-philadelphia-tickets-5-12-2023/event/150593637/?quantity=2")
+	c.OnHTML(("div.sc-jYKCQm.fidnen"), func(e *colly.HTMLElement) {
+		fmt.Println("here")
+	})
+	c.Visit("https://www.stubhub.com/taylor-swift-philadelphia-tickets-5-12-2023/event/150593637/?quantity=2")
 
-// }
+}
--- practiceFiles/productPractice.go
+++ practiceFiles/productPractice.go
@@ -1,70 +1,70 @@
-// package main
+package main
 
-// import (
-// 	"encoding/csv"
-// 	"fmt"
-// 	"log"
-// 	"os"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
 
-// 	"github.com/gocolly/colly"
-// )
+	"github.com/gocolly/colly"
+)
 
-// type Product struct {
-// 	name            string
-// 	rating          string
-// 	numberOfRatings int
-// 	price           string
-// 	retailPrice     string
-// }
+type Product struct {
+	name            string
+	rating          string
+	numberOfRatings int
+	price           string
+	retailPrice     string
+}
 
-// func main() {
-// 	c := colly.NewCollector(colly.AllowedDomains())
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Visiting", r.URL)
-// 	})
+func main() {
+	c := colly.NewCollector(colly.AllowedDomains())
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
-// 	// productList := make([]Product, 0)
+	// productList := make([]Product, 0)
 
-// 	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
-// 		// var p Product
+	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
+		// var p Product
 
-// 		e.ForEach("div.a-section.a-spacing-small.a-spacing-top-small", func(_ int, e *colly.HTMLElement) {
-// 			var productName string
-// 			var rating string
-// 			var numberOfRatings string
-// 			var price string
+		e.ForEach("div.a-section.a-spacing-small.a-spacing-top-small", func(_ int, e *colly.HTMLElement) {
+			var productName string
+			var rating string
+			var numberOfRatings string
+			var price string
 
-// 			fName := "amazon.csv"
-// 			file, err := os.Create(fName)
-// 			if err != nil {
-// 				log.Fatalf("Could not create file, err: %q", err)
-// 				return
-// 			}
-// 			defer file.Close()
+			fName := "amazon.csv"
+			file, err := os.Create(fName)
+			if err != nil {
+				log.Fatalf("Could not create file, err: %q", err)
+				return
+			}
+			defer file.Close()
 
-// 			writer := csv.NewWriter(file)
-// 			defer writer.Flush()
-// 			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
+			writer := csv.NewWriter(file)
+			defer writer.Flush()
+			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
 
-// 			if productName == "" {
-// 				// If we can't get any name, we return and go directly to the next element
-// 				return
-// 			}
+			if productName == "" {
+				// If we can't get any name, we return and go directly to the next element
+				return
+			}
 
-// 			rating = e.ChildText("span.a-icon-alt")
-// 			numberOfRatings = e.ChildText("span.a-size-base.puis-light-weight-text.s-link-centralized-style")
+			rating = e.ChildText("span.a-icon-alt")
+			numberOfRatings = e.ChildText("span.a-size-base.puis-light-weight-text.s-link-centralized-style")
 
-// 			price = e.ChildText("span.a-offscreen")
+			price = e.ChildText("span.a-offscreen")
 
-// 			writer.Write([]string{
-// 				productName,
-// 				rating,
-// 				numberOfRatings,
-// 				price,
-// 			})
-// 		})
+			writer.Write([]string{
+				productName,
+				rating,
+				numberOfRatings,
+				price,
+			})
+		})
 
-// 	})
+	})
 
-// 	c.Visit("https://www.amazon.com/s?k=iphone+13&ref=nb_sb_noss_1")
-// }
+	c.Visit("https://www.amazon.com/s?k=iphone+13&ref=nb_sb_noss_1")
+}
